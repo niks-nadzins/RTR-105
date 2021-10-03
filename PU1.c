697e0727e83cd924cd964ef26e13dbde6b77a311
niks-nadzins <73239496+niks-nadzins@users.noreply.github.com>
2021-10-03 17:51:16 +0300

    Add files via upload

---
+++ PU1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+double input1, input2;
+
+printf("Enter 2 numbers:\n");
+scanf("%lf",&input1);
+scanf("%lf",&input2);
+printf("Result: %lf",input1*input2);
+return 0;
+
+}
