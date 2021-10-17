a6b6f076008fe93b19526da8110f8a69665846ff
niks-nadzins <73239496+niks-nadzins@users.noreply.github.com>
2021-10-17 19:50:35 +0300

    Add files via upload

---
+++ PU2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+int a[10], dec, i;
+
+printf("Ievadi: ");
+scanf("%d", &dec);
+
+for(i=0; dec>0; i++){
+
+a[i]=dec%2;
+dec /= 2;
+}
+
+printf("\n");
+
+for(i=i-1; i>=0; i--){
+
+printf("%d",a[i]);
+}
+return 0;
+}
