8976a4acea2298fe8b7a1483c0a352ff75143dae
niks-nadzins <73239496+niks-nadzins@users.noreply.github.com>
2021-11-04 20:00:08 +0200

    Add files via upload

---
+++ PT5.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+int main(){
+
+	printf("Choose data-type: \n1)char\n2)int \n3)long long\n\n");
+	
+	int choice;
+	
+	scanf("%d", &choice);
+	
+	if (choice == 1){
+		
+		char input, result = 1, result_old = 1;
+		
+		printf("Enter number: ");
+		scanf("%lld", &input);
+		
+		for(input; input != 0; input--){
+			
+			result_old = result;
+			
+			result = result*input;
+			
+			if(result_old > result){
+				printf("Impossible with chosen data-type!\n");
+				break;
+			}
+			
+		}
+		if(input == 0){
+			printf("%lld", result);
+		}
+		
+	}
+	if (choice == 2){
+		
+		int input, result = 1, result_old = 1;
+		
+		printf("Enter number: ");
+		scanf("%lld", &input);
+		
+		for(input; input != 0; input--){
+			
+			result_old = result;
+			
+			result = result*input;
+			
+			if(result_old > result){
+				printf("Impossible with chosen data-type!\n");
+				break;
+			}
+			
+		}
+		if(input == 0){
+			printf("%lld", result);
+		}
+		
+	}
+	if (choice == 3){
+		
+		long long int input, result = 1, result_old = 1;
+		
+		printf("Enter number: ");
+		scanf("%lld", &input);
+		
+		for(input; input != 0; input--){
+			
+			result_old = result;
+			
+			result = result*input;
+			
+			if(result_old > result){
+				printf("Impossible with chosen data-type!\n");
+				break;
+			}
+			
+		}
+		if(input == 0){
+			printf("%lld", result);
+		}
+		
+	}
+	
+	return 0;
+
+}
